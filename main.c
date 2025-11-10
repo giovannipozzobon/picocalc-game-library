2611e37a964297fec8bd0c41c1750bbcd5eac456
giovannipozzobon <giovanni.pozzobon@gmail.com>
2025-11-10 23:30:03 +0100

    Fixed the sprite draw and added the movement of sprite hero.

--- main.c
+++ main.c
@@ -3,14 +3,25 @@
 #include "tiles.h"
 #include "gfx_sprite.h"
 #include "sprite.h"
-
+#include "keyboard.h"
 
 #include <stdio.h>
+
+// Variabile globale richiesta da keyboard.c per la gestione dell'interruzione utente
+volatile bool user_interrupt = false;
 
 extern tilemap_t map;
 lcd_t lcd;
 extern const uint16_t sprite_hero[];
 extern const uint16_t sprite_enemy[];
+
+// Stato dei tasti cursore
+typedef struct {
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+} key_state_t;
 
 void on_collision(int id1, int id2) {
     Sprite *a = sprite_get(id1);
@@ -19,10 +30,29 @@ void on_collision(int id1, int id2) {
         printf("Collisione tra tipo %d e %d!\n", a->type, b->type);
 }
 
+void update_key_state(key_state_t *keys) {
+    // Resetta lo stato
+    keys->up = false;
+    keys->down = false;
+    keys->left = false;
+    keys->right = false;
+
+    // Leggi tutti i tasti disponibili nel buffer
+    while (keyboard_key_available()) {
+        char key = keyboard_get_key();
+
+        if (key == KEY_UP) keys->up = true;
+        else if (key == KEY_DOWN) keys->down = true;
+        else if (key == KEY_LEFT) keys->left = true;
+        else if (key == KEY_RIGHT) keys->right = true;
+    }
+}
+
 int main() {
     stdio_init_all();
     lcd_init(&lcd);
-
+    keyboard_init();  // Inizializza la tastiera del PicoCalc
+    keyboard_set_background_poll(true);  // Abilita il polling in background
     sprite_init();
 
     int scroll_x = 0, scroll_y = 0;
@@ -34,7 +64,37 @@ int main() {
     int hero = sprite_create(50, 50, 16, 16, sprite_hero, 1);
     int enemy = sprite_create(10, 155, 16, 16, sprite_enemy, 2);
 
+    key_state_t keys;
+    Sprite *hero_sprite = sprite_get(hero);
+
     while (true) {
+        // Poll manuale della tastiera per input tempo reale
+        keyboard_poll();
+
+        // Leggi lo stato dei tasti
+        update_key_state(&keys);
+
+        // Muovi lo sprite hero in base all'input
+        if (hero_sprite) {
+            hero_sprite->dx = 0;
+            hero_sprite->dy = 0;
+
+            if (keys.up) hero_sprite->dy = -2;
+            if (keys.down) hero_sprite->dy = 2;
+            if (keys.left) hero_sprite->dx = 2;
+            if (keys.right) hero_sprite->dx = -2;
+
+            // Limita il movimento ai bordi dello schermo
+            if (hero_sprite->x + hero_sprite->dx < 0)
+                hero_sprite->dx = -hero_sprite->x;
+            if (hero_sprite->x + hero_sprite->dx + hero_sprite->width > LCD_WIDTH)
+                hero_sprite->dx = LCD_WIDTH - hero_sprite->x - hero_sprite->width;
+            if (hero_sprite->y + hero_sprite->dy < 0)
+                hero_sprite->dy = -hero_sprite->y;
+            if (hero_sprite->y + hero_sprite->dy + hero_sprite->height > LCD_HEIGHT)
+                hero_sprite->dy = LCD_HEIGHT - hero_sprite->y - hero_sprite->height;
+        }
+
         // Disegna la tilemap con scroll
         tilemap_draw(&lcd, &map, scroll_x, scroll_y);
 
